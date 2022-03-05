089c950a580404d057bbec0f759f95360f5ff282
musleh0001 <khan.muslehuddin0001@gmail.com>
2022-03-06 01:50:58 +0600

    update category/models.py and post/models.py

--- category/models.py
+++ category/models.py
@@ -2,5 +2,11 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=24)
-    description = models.TextField(max_length=500)
+    name = models.CharField(max_length=24, unique=True)
+    description = models.TextField(max_length=500, null=True, blank=True)
+    status = models.CharField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
--- post/models.py
+++ post/models.py
@@ -4,3 +4,10 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=250)
     body = models.TextField(max_length=5000)
+    slug = models.SlugField()
+    status = models.CharField()
+    published_on = models.DateTimeField(auto_now_add=True)
+    last_updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
