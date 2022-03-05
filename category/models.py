c5f3cbe0d3047dff537c16a1daf44693cd7b6f6c
musleh0001 <khan.muslehuddin0001@gmail.com>
2022-03-06 01:18:48 +0600

    add models in category and post apps

--- category/models.py
+++ category/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=24)
+    description = models.TextField(max_length=500)
